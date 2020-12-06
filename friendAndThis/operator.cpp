3a1bf9ccd4a598b12f649cec924f21542a07aa37
mohammedghafri <eng.m.ghafri@gmail.com>
2020-12-06 19:34:31 +0200

    classes

---
+++ friendAndThis/operator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+    public:
+        int var;
+        MyClass() { }
+        MyClass(int a)
+        : var(a) { }
+
+        MyClass operator+(MyClass &obj) {
+            MyClass res;
+            cout<<"this from + operator"<<this->var<<endl; // This-> var, will indicate to the first obj.
+            res.var= this->var+obj.var;
+            return res; 
+        }
+};
+
+int main() {
+    MyClass obj1(12), obj2(55);
+    MyClass res = obj2+obj1;
+
+    cout << res.var;
+}
